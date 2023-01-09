8bb3c12e84c8179a7bd6a79a693c2ea19a1d7a92
leonardomg888 <leonardomg888@gmail.com>
2023-01-09 03:36:50 +0100

    listo

---
+++ public/js/moviesAddValidation.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+//------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
+//-------------------DE REGISTRO DE PELÍCULAS------------------//    
+
+$("title").focus()
+
+let inputs = document.querySelectorAll("#formMoviesAdd input")
+inputs = [...inputs, document.que]
+
+
+}
